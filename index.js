79ba30aee037c1e1f843bb289af27f50c6b585a0
G-Jeffrey <jeffreyg2240@gmail.com>
2022-01-10 17:09:29 -0500

    editing user routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const { PrismaClient } = require('@prisma/client');
 const port = process.env.PORT || 8080;
 const prisma = new PrismaClient();
+const users = require('./routes/Users');
 const app = express();
 app.set('trust proxy', 1);
 app.use(express.json());
@@ -16,7 +17,7 @@ app.get('', (req,res)=>{
 app.get(`/api`,  (req, res) => {
   res.json({'up':true});
 })
-app.use('/users', require('./routes/Users'));
+app.use('/users', users);
 // app.use('/orders', require('./routes/Orders'))
 // app.use('/items', require('./routes/Items'))
 
