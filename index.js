2fec52960b3b2b1736e2903e665c96bca439485e
G-Jeffrey <jeffreyg2240@gmail.com>
2022-01-10 16:08:54 -0500

    reverted package changes

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { PrismaClient } = require('@prisma/client')
-
+const port = process.env.PORT || 8080;
 const prisma = new PrismaClient()
 const app = express()
-
+app.set('trust proxy', 1);
+app.use(express.json());
 app.use(bodyParser.json())
 app.use(express.static('public'))
-
+app.use('/users', require('./routes/Users'));
+app.use('/orders', require('./routes/Orders'))
+app.use('/items', require('./routes/Items'))
 app.get(`/api`, async (req, res) => {
   res.json({ up: true })
 })
