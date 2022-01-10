d2664d2aa114ac610d78baa8ee0a92a3822fe399
G-Jeffrey <jeffreyg2240@gmail.com>
2022-01-10 14:16:41 -0500

    changed prisma schema

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 let cors = require("cors");
 const bodyParser = require('body-parser')
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
