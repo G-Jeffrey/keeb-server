2d73d699a6f043459ffdee89b62f905ea5b6ece0
G-Jeffrey <jeffreyg2240@gmail.com>
2022-01-13 00:13:18 -0500

    changed cors policy

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const prisma = new PrismaClient();
 const app = express();
 app.set('trust proxy', 1);
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 app.get('', (req,res)=>{
   res.status(200).json({'msg':'Toki Server'});
